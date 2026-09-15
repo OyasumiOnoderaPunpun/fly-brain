1cb3dddd6c667876d21eccc5c0990f1e97684283
2V9RU Dev <2v9ru@dev.twov9ru>
2026-09-15 05:48:12 +0530

    Update Groq Model ID to Mixtral

--- server.py
+++ server.py
@@ -95,7 +95,7 @@ def talk_to_fly():
     # Generate a response using Groq
     try:
         completion = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="mixtral-8x7b-32768",
             messages=chat_histories[player_name],
             temperature=0.7,
             max_completion_tokens=80,
